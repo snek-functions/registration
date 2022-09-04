f701e7849751a1b3e2127e3e1de4f6da1e7b48c4
Florian Kleber <kleberbaum@erebos.xyz>
2022-09-04 13:22:08 +0000

    chore: adjust template
     - delete exampleLogin
     - update factory
     - adjust package

--- src/factory.ts
+++ src/factory.ts
@@ -1,5 +1,11 @@
-import {makeFn} from '@snek-at/functions'
+import { makeFn } from '@snek-at/functions'
+
+export const url = process.env.IS_OFFLINE
+  ? process.env.CODESPACE_NAME
+    ? `https://${process.env.CODESPACE_NAME}-9000.githubpreview.dev/graphql`
+    : 'http://localhost:4000/graphql'
+  : `https://kleberbaum-schett-net-ami-new-vpwp4j6p4x4fw667-3030.githubpreview.dev/graphql`
 
 export const fn = makeFn({
-  url: 'https://<your-snek-functions-url>'
+  url
 })
