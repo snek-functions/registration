fa31266989013e03768677c0cf5278a3df0c5c10
Florian Kleber <kleberbaum@erebos.xyz>
2022-09-06 03:36:28 +0000

    wip

--- src/factory.ts
+++ src/factory.ts
@@ -1,10 +1,11 @@
-import { makeFn } from '@snek-at/functions'
+import {makeFn} from '@snek-at/functions'
+import type {SpawnOptionsWithoutStdio} from 'child_process'
 
 export const url = process.env.IS_OFFLINE
   ? process.env.CODESPACE_NAME
-    ? `https://${process.env.CODESPACE_NAME}-9000.githubpreview.dev/graphql`
-    : 'http://localhost:4000/graphql'
-  : `https://kleberbaum-schett-net-ami-new-vpwp4j6p4x4fw667-3030.githubpreview.dev/graphql`
+    ? `https://${process.env.CODESPACE_NAME}-4030.githubpreview.dev/graphql`
+    : 'http://localhost:4030/graphql'
+  : `${process.env.ENDPOINT_URL_REGISTRATION}`
 
 export const fn = makeFn({
   url
